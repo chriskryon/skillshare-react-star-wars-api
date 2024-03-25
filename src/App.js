14d497879c65444baa0ba35ba6a85e55dd396980
Christopher <contato.christopher@gmail.com>
2024-03-25 15:51:53 -0300

    create component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
+import Item from "./MyItem"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Item name="Chris" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,11 +18,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React here
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/MyItem.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+class Item extends React.Component {
+  clickMe() {
+    console.log("I was clicked.")
+  }
+
+  render() {
+    return (
+      <h1
+        onClick={() => {
+          this.clickMe()
+        }}
+      >
+        Hello, {this.props.name}!
+      </h1>
+    )
+  }
+}
+
+export default Item
